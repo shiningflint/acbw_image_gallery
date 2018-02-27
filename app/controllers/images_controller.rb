153c3c188aee20f7832ab33507e031bc4e874495
Adam Christopher <adam.keristoper@gmail.com>
2018-02-27 23:53:27 +0900

    Fix the controller to return proper json shape

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,10 +1,9 @@
 class ImagesController < ApplicationController
   def index
     if params[:path]
-      aws_load(params[:path])
-      # render plain: @lists.inspect
-      print_regex(@lists, /uploads\/([0-9]+)\/([0-9]+)\/(.+\.jpg)/)
-      render json: @lists
+      @lists = aws_load(params[:path])
+      @images = print_regex(@lists, /uploads\/([0-9]+)\/([0-9]+)\/(.+\.jpg)/)
+      render json: @images
     else
       @lists = []
       render plain: params.inspect
@@ -13,17 +12,23 @@ class ImagesController < ApplicationController
 
   private
     def aws_load(path)
-      @s3 = Aws::S3::Resource.new
-      @acbw_bucket = @s3.bucket(ENV['AWS_S3_BUCKET'])
-      @lists = @acbw_bucket.objects(prefix: path).collect(&:key)
+      s3 = Aws::S3::Resource.new
+      acbw_bucket = s3.bucket(ENV['AWS_S3_BUCKET'])
+      return acbw_bucket.objects(prefix: path).collect(&:key)
     end
 
     def print_regex(list, pattern)
+      images = Hash.new
+
       list.each do |item|
         pattern.match(item)
         if pattern.match(item) != nil
-          puts "#{pattern.match(item)[1]}, #{pattern.match(item)[2]}, #{pattern.match(item)[3]}"
+          images[pattern.match(item)[1]] = { pattern.match(item)[2] => [] } if images[pattern.match(item)[1]].nil?
+          images[pattern.match(item)[1]][pattern.match(item)[2]] = [] if images[pattern.match(item)[1]][pattern.match(item)[2]].nil?
+          images[pattern.match(item)[1]][pattern.match(item)[2]] << pattern.match(item)[3]
         end
       end
+
+      images
     end
 end
